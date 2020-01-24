57767094b0e6d7b2abd544cbf03a40b0f136d02c
Luigi <luigitosin@hotmail.com>
2020-01-24 20:56:12 -0300

    "Inicialização de repositorio"

---
+++ src/main/java/com/lughtech/grana/GranaApplication.java
@@ -0,0 +1,13 @@
+package com.lughtech.grana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lughtech/grana/GranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lughtech.grana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
