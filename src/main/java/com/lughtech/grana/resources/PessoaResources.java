a42a9916b35c52703f26fa5ea990ef2a2a336023
Luigi <luigitosin@gmail.com>
2020-01-24 22:14:19 -0300

    Implementado primeiro Service e Repository

--- src/main/java/com/lughtech/grana/resources/PessoaResources.java
+++ src/main/java/com/lughtech/grana/resources/PessoaResources.java
@@ -1,16 +1,25 @@
 package com.lughtech.grana.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lughtech.grana.domain.Pessoa;
+import com.lughtech.grana.services.PessoaService;
 
 @RestController
 @RequestMapping(value = "/pessoa")
 public class PessoaResources {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "teste";
-	}
+	@Autowired
+	private PessoaService pessoaService;
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Pessoa obj = pessoaService.buscarPessoa(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
