5633e5d99ff1296098ee7b18e5f7abcd499b2260
Luigi <luigitosin@gmail.com>
2020-07-15 00:20:30 -0300

    Remoção de arquivos de configuração

--- src/main/java/com/lughtech/grana/seguranca/UtilitarioJWT.java
+++ src/main/java/com/lughtech/grana/seguranca/UtilitarioJWT.java
@@ -2,7 +2,6 @@ package com.lughtech.grana.seguranca;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -12,16 +11,11 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class UtilitarioJWT {
 
-	@Value("${jwt.secret}")
-	private String segredo;
-
-	@Value("${jwt.expiration}")
-	private Long expiracao;
+	private String segredo = "SequenciaDeCaracteresParaAssinarToken";
+	private Long expiracao = 86400000L;
 
 	public String generateToken(String nomeUsuario, Integer idUsuario) {
-		return Jwts.builder().setSubject(nomeUsuario).claim("user_id", idUsuario)
-				.setExpiration(new Date(System.currentTimeMillis() + expiracao))
-				.signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
+		return Jwts.builder().setSubject(nomeUsuario).claim("user_id", idUsuario).setExpiration(new Date(System.currentTimeMillis() + expiracao)).signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
 	}
 
 	public boolean tokenValido(String token) {
